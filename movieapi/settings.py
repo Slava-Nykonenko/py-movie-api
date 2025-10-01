67bfbb5735b8e3fa1e588d2791a7b829b66e120e
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-10-01 12:55:59 +0100

    started app 'user', created User model inside it, and created Movie model in the cinema app

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import user
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cinema",
+    "user"
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
