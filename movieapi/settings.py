794933939f20277876881e2ca32c13e52799f8f7
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-10-01 18:13:57 +0100

    implemented Movie model and MovieSerializer, created apiviews

--- movieapi/settings.py
+++ movieapi/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
     "cinema",
-    "user"
+    "user",
 ]
 
 AUTH_USER_MODEL = "user.User"
